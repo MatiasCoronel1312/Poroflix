bc4865154d344419c9e69374cd880790f1b819e2
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2026-05-14 00:10:47 -0300

    creamos carousel

--- Front/src/pages/DetailMovie.jsx
+++ Front/src/pages/DetailMovie.jsx
@@ -19,16 +19,32 @@ export const DetailMovie = () => {
     <div className="w-full flex">
       <div className="w-[50%]">
         {/* <img className="p-3 rounded-4xl" src={movie.img}></img> */}
-        <iframe width="560" height="315" className="rounded-4xl" src={movie.trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <button className="bt">Agregar al carrito</button>
-        <button >Comprar</button>
+        <iframe
+          width="560"
+          height="315"
+          className="rounded-4xl"
+          src={movie.trailer}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+        <div className="flex justify-around p-3">
+          <button className="inline-flex h-12 animate-background-shine items-center justify-center rounded-md border border-gray-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-gray-400 transition-colors focus:outline-none focus:ring-2  focus:ring-offset-gray-50 hover:cursor-pointer">
+            Agregar al carrito
+          </button>
+          <button className="inline-flex h-12 animate-background-shine items-center justify-center rounded-md border border-gray-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-gray-400 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-gray-50 hover:cursor-pointer">
+            Comprar
+          </button>
+        </div>
       </div>
-      <div className="w-[50%] p-3">
-        <div className="text-2xl font-bold">{movie.title}</div>
-        <div className="">{movie.detail}</div>
-        <div>Director: {movie.director}</div>
-        <div>Categoria: {movie.category}</div>
-        <div>Duracion: {movie.duration}min.</div>
+      <div className="w-[50%] px-5 text-gray-400">
+        <div className="text-2xl font-bold text-white">{movie.title}</div>
+        <p className="text-left">{movie.detail}</p>
+        <div className="text-left my-1.5">Director: {movie.director}</div>
+        <div className="text-left">Categoria: {movie.category}</div>
+        <div className="text-left my-1.5">Duracion: {movie.duration}min.</div>
       </div>
     </div>
   );
--- Front/src/pages/Home.jsx
+++ Front/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Card } from "../components/Card";
+import { CarouselHome } from "@/components/CarouselHome";
 
 export const Home = () => {
   const [movies, setMovies] = useState([])
@@ -14,19 +15,24 @@ export const Home = () => {
   
 
   return (
-    <div className="w-full py-10 flex flex-col md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-3 gap-4 justify-between">
-      {movies.map((pelicula) => (
-        <Card
-          id={pelicula.id}
-          key={pelicula.id}
-          title={pelicula.title}
-          director={pelicula.director}
-          category={pelicula.category}
-          description={pelicula.description}
-          duration={pelicula.duration}
-          img={pelicula.img}
-        />
-      ))}
-    </div>
+    <main className="w-full">
+      <div className="w-full h-80 flex items-center justify-center pt-10">
+        <CarouselHome movies={movies}/>
+      </div>
+      <div className="w-full py-10 flex flex-col md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-3 gap-4 justify-between">
+        {movies.map((pelicula) => (
+          <Card
+            id={pelicula.id}
+            key={pelicula.id}
+            title={pelicula.title}
+            director={pelicula.director}
+            category={pelicula.category}
+            description={pelicula.description}
+            duration={pelicula.duration}
+            img={pelicula.img}
+          />
+        ))}
+      </div>
+    </main>
   );
 };
